ecb3286c40c6ac2adb6549697b1932383a50842c
Kaya-Sem <kayasemvc@gmail.com>
2024-07-02 23:07:06 +0200

    moved plugins to own files and general maintenance

--- init.lua
+++ init.lua
@@ -22,19 +22,8 @@ vim.opt.inccommand = 'split' -- substitution live preview
 vim.opt.hlsearch = true
 vim.opt.scrolloff = 10
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
 require('keymaps')
 require('autocommands')
---  See `:help wincmd` for a list of all window commands
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -59,17 +48,6 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
     'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -77,12 +55,6 @@ require('lazy').setup({
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },
 
-    --   -- Here is a more advanced example where we pass configuration
-    -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-    --    require('gitsigns').setup({ ... })
-
-    -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-    --
     -- This is often very useful to both group configuration, as well as handle
     -- lazy loading plugins that don't need to be loaded immediately at startup.
     --
@@ -150,12 +122,8 @@ require('lazy').setup({
         { -- If encountering errors, see telescope-fzf-native README for installation instructions
           'nvim-telescope/telescope-fzf-native.nvim',
 
-          -- `build` is used to run some command when the plugin is installed/updated.
-          -- This is only run then, not every time Neovim starts up.
           build = 'make',
 
-          -- `cond` is a condition used to determine whether this plugin should be
-          -- installed and loaded.
           cond = function()
             return vim.fn.executable 'make' == 1
           end,
@@ -165,25 +133,6 @@ require('lazy').setup({
         { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
       },
       config = function()
-        -- Telescope is a fuzzy finder that comes with a lot of different things that
-        -- it can fuzzy find! It's more than just a "file finder", it can search
-        -- many different aspects of Neovim, your workspace, LSP, and more!
-        --
-        -- The easiest way to use Telescope, is to start by doing something like:
-        --  :Telescope help_tags
-        --
-        -- After running this command, a window will open up and you're able to
-        -- type in the prompt window. You'll see a list of `help_tags` options and
-        -- a corresponding preview of the help.
-        --
-        -- Two important keymaps to use while in Telescope are:
-        --  - Insert mode: <c-/>
-        --  - Normal mode: ?
-        --
-        -- This opens a window that shows you all of the keymaps for the current
-        -- Telescope picker. This is really useful to discover what Telescope can
-        -- do as well as how to actually do it!
-
         -- [[ Configure Telescope ]]
         -- See `:help telescope` and `:help telescope.setup()`
         require('telescope').setup {
@@ -422,139 +371,6 @@ require('lazy').setup({
       end,
     },
 
-    { -- Autoformat
-      'stevearc/conform.nvim',
-      lazy = false,
-      keys = {
-        {
-          '<leader>f',
-          function()
-            require('conform').format { async = true, lsp_fallback = true }
-          end,
-          mode = '',
-          desc = 'format buffer',
-        },
-      },
-      opts = {
-        notify_on_error = true,
-        format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
-          -- have a well standardized coding style. You can add additional
-          -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
-          return {
-            timeout_ms = 500,
-            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-          }
-        end,
-      },
-    },
-
-    { -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      event = 'InsertEnter',
-      dependencies = {
-        {
-          'L3MON4D3/LuaSnip',
-          build = (function()
-            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-              return
-            end
-            return 'make install_jsregexp'
-          end)(),
-          dependencies = {
-            -- `friendly-snippets` contains a variety of premade snippets.
-            --    See the README about individual language/framework/plugin snippets:
-            --    https://github.com/rafamadriz/friendly-snippets
-            -- {
-            --   'rafamadriz/friendly-snippets',
-            --   config = function()
-            --     require('luasnip.loaders.from_vscode').lazy_load()
-            --   end,
-            -- },
-          },
-        },
-        'saadparwaiz1/cmp_luasnip',
-
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-      },
-      config = function()
-        -- :help cmp
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
-        luasnip.config.setup {}
-
-        cmp.setup {
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
-          },
-          completion = { completeopt = 'menu,menuone,noinsert' },
-
-          -- For an understanding of why these mappings were
-          -- chosen, you will need to read `:help ins-completion`
-          --
-          -- No, but seriously. Please read `:help ins-completion`, it is really good!
-          mapping = cmp.mapping.preset.insert {
-            -- Select the [n]ext item
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            -- Select the [p]revious item
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-            -- Scroll the documentation window [b]ack / [f]orward
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-            -- Accept ([y]es) the completion.
-            --  This will auto-import if your LSP supports it.
-            --  This will expand snippets if the LSP sent a snippet.
-            ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-            -- If you prefer more traditional completion keymaps,
-            -- you can uncomment the following lines
-            --['<CR>'] = cmp.mapping.confirm { select = true },
-            --['<Tab>'] = cmp.mapping.select_next_item(),
-            --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-            -- Manually trigger a completion from nvim-cmp.
-            --  Generally you don't need this, because nvim-cmp will display
-            --  completions whenever it has completion options available.
-            ['<C-Space>'] = cmp.mapping.complete {},
-
-            -- Think of <c-l> as moving to the right of your snippet expansion.
-            --  So if you have a snippet that's like:
-            --  function $name($args)
-            --    $body
-            --  end
-            --
-            -- <c-l> will move you to the right of each of the expansion locations.
-            -- <c-h> is similar, except moving you backwards.
-            ['<C-l>'] = cmp.mapping(function()
-              if luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-              end
-            end, { 'i', 's' }),
-            ['<C-h>'] = cmp.mapping(function()
-              if luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-              end
-            end, { 'i', 's' }),
-
-            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-          },
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'path' },
-          },
-        }
-      end,
-    },
-
-
     {
       "catppuccin/nvim",
       priority = 1000,
@@ -563,37 +379,6 @@ require('lazy').setup({
       end,
     },
 
-    { -- Collection of various small independent plugins/modules
-      'echasnovski/mini.nvim',
-      config = function()
-        -- Better Around/Inside textobjects
-        --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [']quote
-        --  - ci'  - [C]hange [I]nside [']quote
-        require('mini.ai').setup { n_lines = 500 }
-
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require('mini.surround').setup()
-
-        local statusline = require 'mini.statusline'
-
-        statusline.setup { use_icons = vim.g.have_nerd_font }
-
-        -- You can configure sections in the statusline by overriding their
-        -- default behavior. For example, here we set the section for
-        -- cursor location to LINE:COLUMN
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function()
-          return '%2l:%-2v'
-        end
-      end,
-    },
     { -- Highlight, edit, and navigate code
       'nvim-treesitter/nvim-treesitter',
       build = ':TSUpdate',
