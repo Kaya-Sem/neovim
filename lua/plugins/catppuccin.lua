562ad924498adc144b4eb880843638d5657335d2
Kaya-Sem <kayasemvc@gmail.com>
2024-08-25 11:52:24 +0200

    update theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,29 +1,34 @@
 return {
-    "catppuccin/nvim",
+    "folke/tokyonight.nvim",
     priority = 1000,
     init = function()
-        -- set colorscheme based on the time of day
+        -- Extract TokyoNight themes to variables
+        local light_theme = 'tokyonight-day'
+        local dark_theme = 'tokyonight-night'
+
+        -- Set colorscheme based on the time of day
         local function set_colorscheme()
             local hour = tonumber(os.date("%H"))
             if hour >= 17 or hour < 6 then
-                vim.cmd.colorscheme 'catppuccin-mocha'
+                vim.cmd.colorscheme(dark_theme)
             else
-                vim.cmd.colorscheme 'catppuccin-latte'
+                vim.cmd.colorscheme(light_theme)
             end
         end
 
         set_colorscheme()
 
+        -- Set up which-key bindings using the themes
         require("which-key").add({
             {
                 "<leader>ul",
-                ':colorscheme catppuccin-latte<CR>',
+                ':colorscheme ' .. light_theme .. '<CR>',
                 desc = "[L]ightmode",
                 mode = "n"
             },
             {
                 "<leader>ud",
-                ':colorscheme catppuccin-mocha<CR>',
+                ':colorscheme ' .. dark_theme .. '<CR>',
                 desc = "[D]arkmode",
                 mode = "n"
             },
