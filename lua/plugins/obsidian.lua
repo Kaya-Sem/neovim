49af9b30853fb44cd91e07e184dc467f24cc3c44
Kaya-Sem <kayasemvc@gmail.com>
2025-04-24 20:24:11 +0200

    removed stupid commands

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -20,19 +20,6 @@ return {
         t = { "<cmd>ObsidianTOC<CR>", "[T]OC" },
         ["<CR>"] = { "<cmd>ObsidianFollowLink<CR>", "[E]nter Link" },
       },
-      g = {
-        name = "Go to",
-        o = {
-          name = "Obsidian",
-          o = { "<cmd>ObsidianOpen<CR>", "[O]pen in Obsidian" },
-          s = { "<cmd>ObsidianSearch<CR>", "[S]earch Obsidian" },
-          q = {
-            s = { "<cmd>ObsidianQuickSwitch<CR>", "[Q]uickswitch" },
-          },
-          t = { "<cmd>ObsidianTOC<CR>", "[T]OC" },
-          ["<CR>"] = { "<cmd>ObsidianFollowLink<CR>", "[E]nter Link" },
-        },
-      },
     }, { prefix = "<leader>" })
   end,
 
