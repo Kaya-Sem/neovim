3fa9f4f4a6c448f1ef26eaf5df516938185fc3af
Kaya-Sem <kayasemvc@gmail.com>
2025-04-27 20:06:03 +0200

    update

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -90,6 +90,44 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   end,
 })
 
+
+
+
+
+vim.api.nvim_create_user_command("Pinright", function(opts)
+  opts = opts or {}
+  local line1 = opts.line1 or -1
+  local line2 = opts.line2 or -1
+
+  if line1 < 0 or line2 < 0 then
+    print("Invalid range, aborting.")
+    return
+  end
+
+  local source_win = vim.api.nvim_get_current_win()
+  local source_buf = vim.api.nvim_get_current_buf()
+
+  local lines = vim.api.nvim_buf_get_lines(source_buf, line1 - 1, line2, false)
+
+  local source_ft = vim.bo[source_buf].filetype
+
+  vim.cmd("vsplit")
+  local new_buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_win_set_buf(0, new_buf)
+
+  vim.api.nvim_buf_set_lines(new_buf, 0, -1, false, lines)
+
+  vim.bo[new_buf].buftype = "nofile"
+  vim.bo[new_buf].bufhidden = "wipe"
+  vim.bo[new_buf].swapfile = false
+  vim.bo[new_buf].filetype = source_ft --
+
+  vim.api.nvim_set_current_win(source_win)
+end, { range = true })
+
+
+
+
 vim.api.nvim_create_user_command("Pintop", function(opts)
   opts = opts or {}
   local line1 = opts.line1 or -1
